a27923009d402759d31461c1b78289fab486f3ff
fenqyy <73462598+fenqyy@users.noreply.github.com>
2022-10-19 23:44:02 +0200

    Add files via upload

---
+++ lab2.py
@@ -0,0 +1,63 @@
+import matplotlib.pyplot as plt
+from matplotlib.image import imread
+from matplotlib.pyplot import imshow
+from enum import Enum
+
+class ColorModel(Enum):
+    rgb = 0
+    hsv = 1
+    hsi = 2
+    hsl = 3
+    gray = 4
+
+class BaseImage:
+    data: np.ndarray  # tensor przechowujacy piksele obrazu
+    color_model: ColorModel  # atrybut przechowujacy biezacy model barw obrazu
+
+    def __init__(self, path: str) -> None:
+        self.path = path
+        self.data = imread(path)
+        pass
+
+    def save_img(self, path: str) -> None:
+        imsave('image.jpg', self.data)
+        pass
+
+    def show_img(self) -> None:
+        plt.imshow(self.data)
+        plt.show()
+        pass
+
+    def get_layer(self, layer_id: int) -> 'BaseImage':
+        """
+        metoda zwracajaca warstwe o wskazanym indeksie
+        """
+        pass
+
+    def to_hsv(self) -> 'BaseImage':
+        """
+        metoda dokonujaca konwersji obrazu w atrybucie data do modelu hsv
+        metoda zwraca nowy obiekt klasy image zawierajacy obraz w docelowym modelu barw
+        """
+        pass
+
+    def to_hsi(self) -> 'BaseImage':
+        """
+        metoda dokonujaca konwersji obrazu w atrybucie data do modelu hsi
+        metoda zwraca nowy obiekt klasy image zawierajacy obraz w docelowym modelu barw
+        """
+        pass
+
+    def to_hsl(self) -> 'BaseImage':
+        """
+        metoda dokonujaca konwersji obrazu w atrybucie data do modelu hsl
+        metoda zwraca nowy obiekt klasy image zawierajacy obraz w docelowym modelu barw
+        """
+        pass
+
+    def to_rgb(self) -> 'BaseImage':
+        """
+        metoda dokonujaca konwersji obrazu w atrybucie data do modelu rgb
+        metoda zwraca nowy obiekt klasy image zawierajacy obraz w docelowym modelu barw
+        """
+        pass
